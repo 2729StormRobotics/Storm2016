fe1ee1f219e90b1ec5b5bd80a6852ab431e5929b
Jordan Davis <115517@lrstudents.org>
2016-02-17 17:52:13 -0500

    Updated PositionCenter

---
+++ src/org/usfirst/frc/team2729/robot/autoModes/PositionCenter.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team2729.robot.autoModes;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.team2729.robot.autoModes.BreachDefenseAuto;
+import org.usfirst.frc.team2729.robot.commands.Shoot;
+import org.usfirst.frc.team2729.robot.commands.ShootTiltToAngle;
+import org.usfirst.frc.team2729.robot.commands.Turn;
+
+public class PositionCenter extends CommandGroup{
+	public PositionCenter(){
+		addSequential(new BreachDefenseAuto(100, .20));
+		addSequential(new Turn(30));
+		addSequential(new ShootTiltToAngle(45));
+		addSequential(new Shoot());
+	}
+}
